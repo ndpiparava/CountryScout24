ef1d20f31bab400c4eca478cd56954af72407d97
Nitin Piparava <ndpiparava@gmail.com>
2025-10-09 17:21:02 +0200

    CSCOUT24 - 000 feat : Add country flag component

---
+++ src/components/atoms/CountryFlag/CountryFlag.spec.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {render} from '@testing-library/react-native';
+import CountryFlag from './index';
+import {MockAppContainer} from '@CountryScout24/__specs__/mocks/MockAppContainer';
+
+const COUNTRY_FLAG_IMAGE_TEST_ID = 'COUNTRY_FLAG_IMAGE_TEST_ID';
+
+const renderComponent = (props = {}) => {
+  const defaultProps = {
+    src: 'https://example.com/flag.png',
+    ...props,
+  };
+  return render(
+    <MockAppContainer>
+      <CountryFlag {...defaultProps} testID={COUNTRY_FLAG_IMAGE_TEST_ID} />
+    </MockAppContainer>,
+  );
+};
+
+describe('CountryFlag Component', () => {
+  const defaultSrc = 'https://example.com/flag.png';
+
+  it('renders correctly with given src', () => {
+    const {getByTestId} = renderComponent();
+    const image = getByTestId(COUNTRY_FLAG_IMAGE_TEST_ID);
+    expect(image.props.source.uri).toBe(defaultSrc);
+  });
+
+  it('applies default size if no size is provided', () => {
+    const {getByTestId} = renderComponent();
+    const image = getByTestId(COUNTRY_FLAG_IMAGE_TEST_ID);
+    const imageStyle = Array.isArray(image.props.style)
+      ? Object.assign({}, ...image.props.style)
+      : image.props.style;
+
+    expect(imageStyle.width).toBe(50);
+    expect(imageStyle.height).toBe(30);
+  });
+
+  it('applies custom size if size is provided', () => {
+    const {getByTestId} = renderComponent({size: 100});
+    const image = getByTestId(COUNTRY_FLAG_IMAGE_TEST_ID);
+    const imageStyle = Array.isArray(image.props.style)
+      ? Object.assign({}, ...image.props.style)
+      : image.props.style;
+
+    expect(imageStyle.width).toBe(100);
+    expect(imageStyle.height).toBe(60);
+  });
+});
---
+++ src/components/atoms/CountryFlag/index.tsx
@@ -0,0 +1,31 @@
+import styled from '@emotion/native';
+import {memo} from 'react';
+
+type CountryFlagProps = {
+  src: string;
+  size?: number;
+  testID?: string;
+};
+
+const DEFAULT_IMAGE_SIZE = 50;
+
+const CountryFlag = ({src, size, testID}: CountryFlagProps) => {
+  const width = size || DEFAULT_IMAGE_SIZE;
+  const height = (size || DEFAULT_IMAGE_SIZE) * 0.6;
+  return (
+    <FlagImg
+      source={{uri: src}}
+      resizeMode="cover"
+      style={{width, height}}
+      testID={testID}
+    />
+  );
+};
+
+export default memo(CountryFlag);
+
+const FlagImg = styled.Image`
+  border-radius: ${({theme}) => theme.radii.sm};
+  margin-right: ${({theme}) => theme.spacing(1)}px;
+  object-fit: cover;
+`;
