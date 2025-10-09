4b0a54d18a876db264b470cd82f7a525d6559999
Nitin Piparava <ndpiparava@gmail.com>
2025-10-09 18:11:00 +0200

    CSCOUT24 - 000 feat : Add info component

---
+++ src/components/atoms/InfoRow/InfoRow.spec.tsx
@@ -0,0 +1,43 @@
+// src/components/molecules/InfoRow/InfoRow.spec.tsx
+import React from 'react';
+import {render} from '@testing-library/react-native';
+import InfoRow from './index';
+import {MockAppContainer} from '@CountryScout24/__specs__/mocks/MockAppContainer';
+import {Text} from 'react-native';
+
+describe('InfoRow Component', () => {
+  const renderComponent = (props = {}) => {
+    const defaultProps = {
+      label: 'Country',
+      value: 'France',
+      icon: <Text>🏳️</Text>,
+      testID: 'info-row',
+      ...props,
+    };
+
+    return render(
+      <MockAppContainer>
+        <InfoRow {...defaultProps} />
+      </MockAppContainer>,
+    );
+  };
+
+  it('renders label and value correctly', () => {
+    const {getByText} = renderComponent({label: 'Capital', value: 'Paris'});
+
+    expect(getByText('Capital')).toBeTruthy();
+    expect(getByText('Paris')).toBeTruthy();
+  });
+
+  it('renders the icon if provided', () => {
+    const {getByText} = renderComponent({icon: <Text>⭐</Text>});
+    expect(getByText('⭐')).toBeTruthy();
+  });
+
+  it('renders correctly with default props', () => {
+    const {getByText} = renderComponent();
+    expect(getByText('Country')).toBeTruthy();
+    expect(getByText('France')).toBeTruthy();
+    expect(getByText('🏳️')).toBeTruthy();
+  });
+});
---
+++ src/components/atoms/InfoRow/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from '@emotion/native';
+
+type InfoRowProps = {
+  label: string;
+  value: string;
+  icon: React.ReactNode;
+  testID?: string;
+};
+
+const InfoRow = ({label, value, icon, testID}: InfoRowProps) => (
+  <Row testID={testID}>
+    <LabelWrapper>
+      {icon && <IconWrapper>{icon}</IconWrapper>}
+      <Label>{label}</Label>
+    </LabelWrapper>
+    <Value>{value}</Value>
+  </Row>
+);
+
+export default InfoRow;
+
+const Row = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding-vertical: 6px;
+`;
+
+const Label = styled.Text`
+  color: ${({theme}) => theme.colors.textSecondary};
+  font-size: ${({theme}) => theme.fontSizes.xl};
+  text-align: left;
+`;
+
+const Value = styled.Text`
+  color: ${({theme}) => theme.colors.textPrimary};
+  font-size: ${({theme}) => theme.fontSizes.lg};
+  font-weight: ${({theme}) => theme.fontWeights.bold};
+`;
+
+const IconWrapper = styled.View`
+  justify-content: center;
+  align-items: center;
+  margin-right: ${({theme}) => theme.spacing(1)}px;
+`;
+
+const LabelWrapper = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
