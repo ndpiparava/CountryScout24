41b717d38ce1f2cf38e1438d31e0a6b539de214b
Nitin Piparava <ndpiparava@gmail.com>
2025-10-09 12:00:43 +0200

    CSCOUT24 - 000 feat : Setup localisation

---
+++ src/translations/de.ts
@@ -0,0 +1,10 @@
+const de: Record<string, string> = {
+  countries: 'Länder',
+  population: 'Bevölkerung',
+  area: 'Fläche',
+  dataLoadError:
+    'Fehler beim Laden der Länderdaten. Bitte versuchen Sie es erneut.',
+  noCountriesFound: 'Keine Länder gefunden.',
+};
+
+export default de;
---
+++ src/translations/en.ts
@@ -0,0 +1,9 @@
+const en: Record<string, string> = {
+  countries: 'Countries',
+  population: 'Population',
+  area: 'Area',
+  dataLoadError: 'Failed to load countries. Please try again.',
+  noCountriesFound: 'No countries found.',
+};
+
+export default en;
