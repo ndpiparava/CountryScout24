5a67723ac00680c2c0453f8c2a5ec09938b7cda2
Nitin Piparava <ndpiparava@gmail.com>
2025-10-14 11:10:27 +0200

    CSCOUT24 - 000 fix : Implement service protocol

--- src/services/countryService.ts
+++ src/services/countryService.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-import {Country} from '@CountryScout24/domain/country';
+import {Country, CountryRepository} from '@CountryScout24/domain/country';
 import {env} from '@CountryScout24/utils/env';
 
-export class CountryService {
+export class CountryService implements CountryRepository {
   async getEuropeanCountries(): Promise<Country[]> {
     const {data} = await axios.get(
       `${env.API_BASE_URL}/region/europe?fields=name,capital,flags,population,area`,
