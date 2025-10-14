dfa903a6a822d26978f3db9fceaa47e1357a0b19
Nitin Piparava <ndpiparava@gmail.com>
2025-10-14 13:40:28 +0200

    CSCOUT24 - 000 fix :  fetch country make it injectable

--- src/shared/hooks/useFetchCountries.ts
+++ src/shared/hooks/useFetchCountries.ts
@@ -1,16 +1,16 @@
 import {useQuery} from '@tanstack/react-query';
 
+import {CountryRepository} from '@CountryScout24/domain/country';
 import {CountryService} from '@CountryScout24/services';
-
-const countryService = new CountryService();
 
 const STALE_TIME = 1000 * 60 * 60; // 1 hour
 const GC_TIME = 1000 * 60 * 60 * 24; // 24 hours, since country data rarely changes
 
-export const useFetchCountries = () => {
+export const useFetchCountries = (repo?: CountryRepository) => {
+   const service = repo ?? new CountryService();
   const result = useQuery({
     queryKey: ['countries'],
-    queryFn: () => countryService.getEuropeanCountries(),
+    queryFn: () => service.getEuropeanCountries(),
     staleTime: STALE_TIME,
     gcTime: GC_TIME,
   });
